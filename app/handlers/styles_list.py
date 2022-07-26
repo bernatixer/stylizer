2ef8cb28c6f3a27d8d226d7f19e0746ad25b1300
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-07-26 10:40:04 +0200

    Adding param

--- app/handlers/styles_list.py
+++ app/handlers/styles_list.py
@@ -9,7 +9,7 @@ class StylesListHandler:
     def __init__(self):
         pass
 
-    def handle(self):
+    def handle(self, db):
         return {"styles": styles_class.STYLES_REPRESENTATIONS}
 
 styles_list_handler = StylesListHandler()
