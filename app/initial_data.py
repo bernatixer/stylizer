df530f8cabb19a8bb9e9fc8c915ad73349d70fe7
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-07-27 15:52:52 +0200

    Adding json logger

--- app/initial_data.py
+++ app/initial_data.py
@@ -6,22 +6,13 @@ from db.session import SessionLocal
 # logging.basicConfig(level=logging.INFO)
 # logger = logging.getLogger(__name__)
 
-from pythonjsonlogger import jsonlogger
-logger = logging.getLogger()
-
-logHandler = logging.FileHandler(filename='/var/log/stylizer_logs.json')
-formatter = jsonlogger.JsonFormatter()
-logHandler.setFormatter(formatter)
-logger.addHandler(logHandler)
-logger.setLevel(logging.INFO)
-
-
 def init() -> None:
     db = SessionLocal()
     # init_db(db)
 
 
 def main() -> None:
+    logger = logging.getLogger()
     logger.info("Creating initial data")
     init()
     logger.info("Initial data created")
--- app/main.py
+++ app/main.py
@@ -4,6 +4,18 @@ from styles.styles import Styles
 
 from routes.router import api_router
 
+import logging
+import json_log_formatter
+
+formatter = json_log_formatter.JSONFormatter()
+
+json_handler = logging.FileHandler(filename='/var/log/stylizer_logs.json')
+json_handler.setFormatter(formatter)
+
+logger = logging.getLogger('my_json')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
+
 app = FastAPI()
 app.include_router(api_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
