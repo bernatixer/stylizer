d6bb8d0484236f9c5deb9fd2ce625416172c12ba
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-07-27 15:29:27 +0200

    Trying DD local logs

--- app/initial_data.py
+++ app/initial_data.py
@@ -3,8 +3,17 @@ import logging
 # from db.init_db import init_db
 from db.session import SessionLocal
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+from pythonjsonlogger import jsonlogger
+logger = logging.getLogger()
+
+logHandler = logging.FileHandler(filename='/var/log/stylizer_logs.json')
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+logger.setLevel(logging.INFO)
 
 
 def init() -> None:
--- app/main.py
+++ app/main.py
@@ -1,20 +1,8 @@
-import logging
 from fastapi import BackgroundTasks, FastAPI, File, Form, UploadFile
 from fastapi.staticfiles import StaticFiles
 from styles.styles import Styles
-from pythonjsonlogger import jsonlogger
 
 from routes.router import api_router
-
-logger = logging.getLogger()
-
-logHandler = logging.FileHandler(filename='/var/log/stylizer_logs.json')
-formatter = jsonlogger.JsonFormatter()
-logHandler.setFormatter(formatter)
-logger.addHandler(logHandler)
-logger.setLevel(logging.INFO)
-
-logger.info('Sign up', extra={'referral_code': '52d6ce'})
 
 app = FastAPI()
 app.include_router(api_router)
