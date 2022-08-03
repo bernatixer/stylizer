7a20466cdcb25e82362e33d01a3a3e5645220cfd
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-08-04 01:44:28 +0200

    Adding first file log

--- app/routes/index/base.py
+++ app/routes/index/base.py
@@ -3,8 +3,19 @@ from handlers.index import IndexHandler
 
 router = APIRouter()
 
+import logging
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger()
+
+logHandler = logging.FileHandler(filename='./tixer-log.json')
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+logger.setLevel(logging.INFO)
 
 @router.get("/")
 def index():
+    logger.info("Hello world log")
     index_handler = IndexHandler()
     return index_handler.handle()
