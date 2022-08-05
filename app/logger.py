9c695aa3e9e8a9517662e5a05b3fa0b665840c12
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-08-05 15:29:27 +0200

    Setting more loggers

--- app/logger.py
+++ app/logger.py
@@ -4,19 +4,38 @@ from pythonjsonlogger import jsonlogger
 
 class Logger:
     def __init__(self):
-        logHandler = logging.FileHandler(filename='/var/log/stylizer.log')
-        formatter = jsonlogger.JsonFormatter("%(asctime)s %(levelname)s %(name)s %(message)s")
-        logHandler.setFormatter(formatter)
-        appLogger = logging.getLogger()
-        appLogger.addHandler(logHandler)
-        appLogger.setLevel(logging.INFO)
-
-        uviCornLogger = logging.getLogger("uvicorn.access")
-        uvicornHandler = logging.FileHandler(filename='/var/log/stylizer.log')
-        uvicornFormatter = jsonlogger.JsonFormatter("%(asctime)s %(levelname)s %(name)s %(message)s")
-        uvicornHandler.setFormatter(uvicornFormatter)
-        uviCornLogger.addHandler(uvicornHandler)
+        setup_log()
+        map_levelname_to_status()
 
         self.logger = appLogger
+
+    def setup_log(self):
+    loggers = [
+        logging.getLogger("uvicorn.access"),
+        logging.getLogger("uvicorn.error"),
+        logging.getLogger("uvicorn"),
+        logging.getLogger(),
+    ]
+    for logger in loggers:
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
+        logger.level = logging.INFO
+        logHandler = logging.FileHandler(filename='/var/log/stylizer.log')
+        formatter = jsonlogger.JsonFormatter(
+            "%(asctime)s %(levelname)s %(name)s %(message)s"
+        )
+        logHandler.setFormatter(formatter)
+        logger.addHandler(logHandler)
+
+    def map_levelname_to_status() -> None:
+        oldFactory = logging.getLogRecordFactory()
+
+        def record_factory(*args: str, **kwargs: str) -> logging.LogRecord:
+            record = oldFactory(*args, **kwargs)
+            record.status = record.levelname
+            return record
+
+        logging.setLogRecordFactory(record_factory)
+
 
 LOG = Logger().logger
--- app/routes/index/base.py
+++ app/routes/index/base.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter
 from handlers.index import IndexHandler
-from logger import LOG
 
 
 router = APIRouter()
 
 @router.get("/")
 def index():
-    LOG.info("Hello world log")
     index_handler = IndexHandler()
     return index_handler.handle()
