c7852676744aa9f7a14125221eb4358720c6ffcf
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-08-05 19:14:46 +0200

    Trying with created timestamps

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,5 @@ RUN pip install -r requirements.txt
 # COPY .env /app/.env
 
 # CMD ["sh", "./app/prestart"]
+# CMD ["gunicorn", "app.main:app", "-workers", "4", "--worker-class", "uvicorn.workers.UvicornWorker", "--bind" "0.0.0.0:80"]
 # CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/logger.py
+++ app/logger.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from config import settings
 from pythonjsonlogger import jsonlogger
@@ -23,15 +24,16 @@ class Logger:
                 logger.removeHandler(handler)
             logger.level = logging.INFO
 
-            logger.info(f"Env: {settings.ENVIRONMENT}")
-            logHandler = logging.FileHandler(filename="/var/log/stylizer.log")
-            # logHandler = logging.StreamHandler()
-            # if not settings.isLocal:
-            #     logHandler = logging.FileHandler(filename="/var/log/stylizer.log")
 
-            formatter = jsonlogger.JsonFormatter(
-                "%(asctime)s %(name)s %(message)s"
-            )
+            logHandler = None
+            if settings.isLocal:
+                logHandler = logging.StreamHandler()
+            else:
+                logHandler = logging.FileHandler(filename="/var/log/stylizer.log")
+
+            formatter = jsonlogger.JsonFormatter("%(name)s %(message)s")
+            # formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s')
+
             logHandler.setFormatter(formatter)
             logger.addHandler(logHandler)
 
@@ -42,9 +44,24 @@ class Logger:
             record = oldFactory(*args, **kwargs)
             record.level = record.levelname
             record.env = settings.ENVIRONMENT
+            record.timestamp = record.created
+            # record.name = record.name
             return record
 
         logging.setLogRecordFactory(record_factory)
+
+
+# class LogJsonFormatter(jsonlogger.JsonFormatter):
+#     def add_fields(self, log_record, record, message_dict):
+#         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+#         if not log_record.get('timestamp'):
+#             # this doesn't use record.created, so it is slightly off
+#             now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+#             log_record['timestamp'] = now
+#         if log_record.get('level'):
+#             log_record['level'] = log_record['level'].upper()
+#         else:
+#             log_record['level'] = record.levelname
 
 
 LOG = Logger().logger
