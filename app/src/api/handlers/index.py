390c95e330b60be7098b8cf5c4775a9a51dba1df
Bernat Torres <bernat.torres.bellido@gmail.com>
2022-10-03 22:11:21 +0200

    Testing everything and deploy time

--- app/src/api/handlers/index.py
+++ app/src/api/handlers/index.py
@@ -2,7 +2,7 @@ from src.core.config import settings
 
 class IndexHandler:
     def __init__(self):
-        self.SUCCESS_RESPONSE = {"status": "OK"}
+        self.SUCCESS_RESPONSE = {"status": "OKI"}
 
     def handle(self):
         return self.SUCCESS_RESPONSE
